e81b232e794726541d971c53f62cef5622964346
Ellen Peeters <elena.peeters.15@gmail.com>
2018-01-29 09:35:55 +0100

    views terug toevoegen

---
+++ Optiflow/Optiflow/Views/HomePage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Optiflow.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HomePage : ContentPage
+	{
+		public HomePage ()
+		{
+			InitializeComponent ();
+            chart1.Source = ImageSource.FromFile("chart1.jpg");
+        }
+	}
+}
---
+++ Optiflow/Optiflow/Views/TabbedMenuPage.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Optiflow.Views
+{
+	public class TabbedMenuPage : TabbedPage
+	{
+		public TabbedMenuPage ()
+		{
+            Page homePage, settingsPage = null;
+
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    homePage = new NavigationPage(new HomePage())
+                    {
+                        Title = "Home"
+                    };
+
+                    settingsPage = new NavigationPage(new SettingsPage())
+                    {
+                        Title = "Settings"
+                    };
+
+                    homePage.Icon = "tab_feed.png";
+                    settingsPage.Icon = "tab_about.png";
+
+                    break;
+                default:
+                    homePage = new HomePage()
+                    {
+                        Title = "Home"
+                    };
+
+                    settingsPage = new SettingsPage()
+                    {
+                        Title = "Settings"
+                    };
+
+                    break;
+            }
+
+            Children.Add(homePage);
+            Children.Add(settingsPage);
+
+            Title = Children[0].Title;
+        }
+
+        protected override void OnCurrentPageChanged()
+        {
+            base.OnCurrentPageChanged();
+            Title = CurrentPage?.Title ?? string.Empty;
+        }
+    }
+}
