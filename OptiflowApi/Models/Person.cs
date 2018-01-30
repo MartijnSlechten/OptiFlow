9bc4c1890498a58e1bd813077efa8504a7b31943
Ellen Peeters <elena.peeters.15@gmail.com>
2018-01-30 11:39:39 +0100

    api voor testdb

---
+++ OptiflowApi/Models/Person.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace OptiflowApi.Models
+{
+    public class Person : TableEntity
+    {
+        public Person(string email)
+        {
+            this.PartitionKey = email;
+            this.RowKey = email;
+            this.Email = email;
+        }
+
+        public Person(string firstName, string lastName, string email)
+        {
+            this.PartitionKey = email;
+            this.RowKey = email;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Email = email;
+        }
+
+        public Person() { }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
